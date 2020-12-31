76336022064af20ce295d5886e30acc348529553
Joel Welling <welling@psc.edu>
2020-12-30 19:17:19 -0500

    Cleaned up some unneeded code.

--- src/jpsort_if.pyx
+++ src/jpsort_if.pyx
@@ -1,10 +1,11 @@
 import numpy as np
 cimport numpy as cnp
-
 from mpi4py.MPI cimport Comm
 from mpi4py.libmpi cimport MPI_Comm, MPI_COMM_WORLD
 
-#DTYPE = np.float64
+"""
+This module docstring does not seem to end up in the python object
+"""
 
 cdef extern from *:
     struct Comm:
@@ -17,9 +18,9 @@ cdef extern from "jpsort.h":
                 size_t size,
                 int (*compar)(const void*, const void*))
 
-cdef int compar(const void* p1, const void* p2):
-  cdef double v1= (<double*>p1)[0];
-  cdef double v2= (<double*>p2)[0];
+cdef int compare_float64(const void* p1, const void* p2):
+  cdef cnp.npy_float64 v1= (<cnp.npy_float64*>p1)[0];
+  cdef cnp.npy_float64 v2= (<cnp.npy_float64*>p2)[0];
   if (v1<v2):
       return -1;
   elif (v1>v2):
@@ -28,15 +29,16 @@ cdef int compar(const void* p1, const void* p2):
       return 0;
 
 def pyjpSort(Comm comm,
-             object array  # a numpy array- how to specify that?
+             cnp.ndarray array
              ):
-    print(array)
-    print(array.dtype)
-    #cdef double[::1] array_view = np.ascontiguousarray(array)
-    #cdef array_view = np.ascontiguousarray(array)
-    cdef cnp.ndarray[double, ndim=1, mode='c'] np_buf = np.ascontiguousarray(array)
-    cdef size_t nmemb = np_buf.shape[0]
-    cdef size_t size = np_buf.itemsize
+    """
+    Will this come out in the docstring?
+    """
+    assert array.flags['C_CONTIGUOUS'], 'Array must be contiguous and in C order'
+    assert array.flags['WRITEABLE'], 'Array is not writeable and so cannot be sorted'
+    assert array.dtype == np.float64, 'Only float64 data is supported so far'
+    cdef size_t nmemb = array.shape[0]
+    cdef size_t size = array.itemsize
     cdef MPI_Comm mpi_comm = comm.ob_mpi
-    return jpSort(mpi_comm, <void *>np_buf.data, nmemb, size, compar)
+    return jpSort(mpi_comm, <void *>array.data, nmemb, size, compare_float64)
 
